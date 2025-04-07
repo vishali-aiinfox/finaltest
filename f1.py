4aab350414f213ea611f60842e71adb80f9a0970
vishali-aiinfox <vishali@aiinfox.com>
2025-04-07 18:28:36 +0530

    Update f1.py

--- f1.py
+++ f1.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def add_numbers():
-    num1 = 04500
+    num1 = 040
     num2 = 5
     result = num1 + num2
     return f"The sum of {num1} and {num2} is {result}."
